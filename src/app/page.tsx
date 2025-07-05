9d2d9cb84b9c4eee9cba8e2dc88bde8bdf93b461
samradhiR <samradhi4320@gmail.com>
2025-07-06 01:43:18 +0530

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import Addtransaction from './addtransaction/page'
-import Listexpense from './listexpenses/page'
-import MonthlyExpensesChart from './monthlyexpense/page';
-import CategoryPieChart from './catagorypiechart/page';
+
 import Dashboard from './Dashboard/page';
-import BudgetPage from './Budget/page';
-import BudgetVsActualChart from './budgetvsactualchar';
+
 import Navbar from './Navbar'
 export default function Home() {
   
